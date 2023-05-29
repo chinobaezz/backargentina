262d348099d94ac696fc81c04e7202dbb3a799e3
chinobaezz <baezalbino@gmail.com>
2023-05-29 09:38:28 -0300

    2back2

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://mgbfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://chino-frontend.web.app/","http://localhost:4200"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
